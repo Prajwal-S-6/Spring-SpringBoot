af380af653696b7c300279e20616c607543b68db
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-03-18 23:36:38 +0530

    1. get all the bean names using getBeanDefinitionNames(); 2. If there are multiple beans which matches, it can be resolved using   -  @Primary   - @Qualifier("qualifierName")

--- learn-spring-framework/src/main/java/com/learnings/learn_spring_framework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/learnings/learn_spring_framework/App02HelloWorldSpring.java
@@ -2,6 +2,8 @@ package com.learnings.learn_spring_framework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
 
     public static void main(String[] args) {
@@ -18,16 +20,24 @@ public class App02HelloWorldSpring {
         Object age = context.getBean("age");
         System.out.println(age);
 
-        Object person = context.getBean("person");
+        Object person = context.getBean(Person.class);
         System.out.println(person);
 
         Object person2 = context.getBean("person2");
         System.out.println(person2);
 
-        Object person3 = context.getBean(Person.class);
+        Object person3 = context.getBean("person3");
         System.out.println(person3);
+
+        Object person4 = context.getBean("person4");
+        System.out.println(person4);
 
         Object address = context.getBean(Address.class);
         System.out.println(address);
+
+        String[] beanNames = context.getBeanDefinitionNames();
+        Arrays.stream(beanNames).forEach(System.out::println);
+        System.out.println(context.getBeanDefinitionCount());
+        System.out.println(context.getBeanDefinition("myName"));
     }
 }
--- learn-spring-framework/src/main/java/com/learnings/learn_spring_framework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/learnings/learn_spring_framework/HelloWorldConfiguration.java
@@ -1,8 +1,10 @@
 package com.learnings.learn_spring_framework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person(String name, int age, Address address) {}
 
@@ -16,6 +18,7 @@ public class HelloWorldConfiguration {
         return "Prajwal";
     }
 
+
 //    @Bean(name = "lastName")
 //    public String lastName() {
 //        return "S";
@@ -27,6 +30,7 @@ public class HelloWorldConfiguration {
     }
 
     @Bean
+    @Primary
     public Person person() {
         return new Person("ravi", 20, new Address("abc", "def"));
     }
@@ -40,12 +44,24 @@ public class HelloWorldConfiguration {
     @Bean(name = "person3")
     public Person person3(String myName, int age, Address address) {  //myName, age, address
         return new Person(myName, age, address);   //using beans in another bean using Bean name as parameter to your Bean method
+    }
+
+    @Bean(name = "person4")
+    public Person person4(String myName, int age, @Qualifier("addressQualifier") Address address) {  //myName, age, address
+        return new Person(myName, age, address);   //using beans in another bean using Bean name as parameter to your Bean method
         // return new Person()
     }
 
     @Bean
+    @Qualifier("addressQualifier")
     public Address address() {
         return new Address("banashankari", "bangalore");
+    }
+
+    @Bean
+    @Primary
+    public Address address2() {
+        return new Address("banashankari2", "bangalore2");
     }
 
 
