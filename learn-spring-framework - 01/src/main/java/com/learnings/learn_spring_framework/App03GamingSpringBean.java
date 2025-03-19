d81ad990f8edd55452362a8a65a773d09312ef06
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-03-19 08:37:38 +0530

    refactoring

--- learn-spring-framework - 01/src/main/java/com/learnings/learn_spring_framework/App03GamingSpringBean.java
+++ learn-spring-framework - 01/src/main/java/com/learnings/learn_spring_framework/App03GamingSpringBean.java
@@ -1,8 +1,51 @@
 package com.learnings.learn_spring_framework;
 
+import com.learnings.learn_spring_framework.game.GamingConsole;
+import com.learnings.learn_spring_framework.game.MarioGame;
+import com.learnings.learn_spring_framework.game.Pacman;
+import com.learnings.learn_spring_framework.game.SuperContraGame;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
+
+@Configuration
 public class App03GamingSpringBean {
+
+    @Bean
+    @Primary
+    public GamingConsole marioGame() {
+        return new MarioGame();
+    }
+
+    @Bean
+    @Qualifier("pacmanGame")
+    public GamingConsole pacmanGame() {
+        return new Pacman();
+    }
+
+    @Bean
+    public GamingConsole superContraGame() {
+        return new SuperContraGame();
+    }
+
+    @Bean
+    @Primary
+    GameRunner gameRunner1() {
+        return new GameRunner(marioGame());
+    }
+
+    @Bean
+    GameRunner gameRunner2(@Qualifier("pacmanGame") GamingConsole pacmanGame) {
+        return new GameRunner(pacmanGame);
+    }
+
+    @Bean
+    GameRunner gameRunner3() {
+        return new GameRunner(superContraGame());
+    }
 
     public static void main(String[] args) {
 
@@ -12,7 +55,7 @@ public class App03GamingSpringBean {
 //        var gameRunner = new GameRunner(pacmanGame); // 1. object creation + 2. Wiring dependencies
 //        gameRunner.run();
 
-        try(var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+        try(var context = new AnnotationConfigApplicationContext(App03GamingSpringBean.class)) {
 
 //            var gameRunner1 = context.getBean(GameRunner.class);
 //            gameRunner1.run();
