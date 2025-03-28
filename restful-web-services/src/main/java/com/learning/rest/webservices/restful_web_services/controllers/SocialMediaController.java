773125db47b750e9f3efaf5381d1f497b85055b6
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-03-28 08:18:00 +0530

    @JSONProperty("some_value") @JSONIgnore @JsonIgnoreProperties("field1", "field2")
    Dynamic filter
    @JsonFilter("filter-name")
    MappingJacksonValue
    SimpleBeanPropertyFilter

--- restful-web-services/src/main/java/com/learning/rest/webservices/restful_web_services/controllers/SocialMediaController.java
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restful_web_services/controllers/SocialMediaController.java
@@ -3,6 +3,8 @@ package com.learning.rest.webservices.restful_web_services.controllers;
 import com.learning.rest.webservices.restful_web_services.dao.UserDaoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,12 +23,16 @@ public class SocialMediaController {
     }
 
     @GetMapping(value = "users/{id}")
-    public Users getUsersById(@PathVariable int id) {
+    public EntityModel<Users> getUsersById(@PathVariable int id) {
         Users user = userDaoService.findOne(id);
         if(user == null) {
             throw new UserNotFoundException("User Not Found with id: " + id);
         }
-        return user;
+
+        EntityModel<Users> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getUsers());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
     }
 
     @PostMapping(value = "users", consumes = "application/json", produces = "application/json")
--- restful-web-services/src/main/java/com/learning/rest/webservices/restful_web_services/controllers/Users.java
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restful_web_services/controllers/Users.java
@@ -1,11 +1,13 @@
 package com.learning.rest.webservices.restful_web_services.controllers;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
-public record Users(int id, @Size(min=2, message = "Name should be atleast 2 characters") String name, @Past(message = "Birth date cannot be future date") LocalDate birthDate) {
+
+public record Users(int id, @Size(min=2, message = "Name should be atleast 2 characters") @JsonProperty("user_name") String name, @Past(message = "Birth date cannot be future date") LocalDate birthDate) {
 
 
 }
