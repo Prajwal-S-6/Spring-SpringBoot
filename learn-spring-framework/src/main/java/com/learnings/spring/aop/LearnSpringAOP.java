3a7020ea8aec7c1f44b833810cf6a8424ac8e2bf
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-02-28 13:12:33 +0530

    spring Add Maven Compiler Plugin and new LearnSpringAOP class

--- learn-spring-framework/src/main/java/com/learnings/spring/aop/LearnSpringAOP.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/aop/LearnSpringAOP.java
@@ -2,9 +2,31 @@ package com.learnings.spring.aop;
 
 import org.springframework.stereotype.Component;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@SomeAnnotation
 class Person {
     String name;
     int age;
+
+    public void personClassMethod() {
+        System.out.println("PersonClassMethod");
+    }
+}
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@interface SomeAnnotation {
+
+}
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@interface SomeAnnotation2 {
+
 }
 
 @Component
@@ -26,6 +48,17 @@ public class LearnSpringAOP {
     public void method4(Person arg1) {
         System.out.println("method4");
     }
+
+    @SomeAnnotation2
+    public void method5(Person arg1) {
+        System.out.println("method4");
+    }
+
+
+
+
+
+
 
 
 }
