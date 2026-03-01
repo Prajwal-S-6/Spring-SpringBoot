0bbe1402f85d1abc6ec9d2fbc7a14eb85e8ebaca
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-01 07:36:35 +0530

    Refactor Main class to use SingletonBean and PrototypeBean; rename BeanB to PrototypeBean

---
+++ learn-spring-framework/src/main/java/com/learnings/spring/fundamentals/ApplicationScopeBean.java
@@ -0,0 +1,9 @@
+package com.learnings.spring.fundamentals;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@Component
+@ApplicationScope
+public class ApplicationScopeBean {
+}
