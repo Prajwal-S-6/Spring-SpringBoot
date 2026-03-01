de3706179b190e1ab26cb4f00fff7cf24512e937
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-01 07:31:50 +0530

    spring Add spring-boot-starter-web dependency to pom.xml

---
+++ learn-spring-framework/src/main/java/com/learnings/spring/fundamentals/BeanC.java
@@ -0,0 +1,11 @@
+package com.learnings.spring.fundamentals;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class BeanC {
+
+}
---
+++ learn-spring-framework/src/main/java/com/learnings/spring/fundamentals/BeanD.java
@@ -0,0 +1,9 @@
+package com.learnings.spring.fundamentals;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class BeanD {
+}
