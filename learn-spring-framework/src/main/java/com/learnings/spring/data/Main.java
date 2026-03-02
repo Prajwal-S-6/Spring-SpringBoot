b9d786d38144789fc0b74e452983e45865e24ab6
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-02 10:14:48 +0530

    Add UserDaoJpa interface for JPA repository support

--- learn-spring-framework/src/main/java/com/learnings/spring/data/Main.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/data/Main.java
@@ -1,12 +1,29 @@
 package com.learnings.spring.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+
+    @Autowired
+    UserService userService;
 
     static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+//            userService.noRollbackForCheckedException();
+//            userService.rollbackForCheckedException();
+//            userService.rollbackForUnCheckedException();
+            userService.noRollbackForUnCheckedException();
+        } catch (Exception e){
+
+        }
     }
 }
--- learn-spring-framework/src/main/java/com/learnings/spring/data/UserService.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/data/UserService.java
@@ -1,8 +1,10 @@
 package com.learnings.spring.data;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Service
@@ -24,5 +26,25 @@ public class UserService {
 
     public List<Users> getAllUsers() {
         return userDaoJpa.findAll();
+    }
+
+    @Transactional
+    public void noRollbackForCheckedException() throws SQLException {
+        throw new SQLException();
+    }
+
+    @Transactional(rollbackOn = SQLException.class)
+    public void rollbackForCheckedException() throws SQLException {
+        throw new SQLException();
+    }
+
+    @Transactional()
+    public void rollbackForUnCheckedException() {
+        throw new RuntimeException();
+    }
+
+    @Transactional(dontRollbackOn = RuntimeException.class)
+    public void noRollbackForUnCheckedException() {
+        throw new RuntimeException();
     }
 }
