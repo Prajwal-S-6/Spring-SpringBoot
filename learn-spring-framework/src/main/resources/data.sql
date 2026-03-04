c0f41091dca8b6754d2060ec05abdd6d685e65c5
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-04 07:00:38 +0530

    Add MvcMain class to initialize Spring Boot application

--- learn-spring-framework/src/main/resources/data.sql
+++ learn-spring-framework/src/main/resources/data.sql
@@ -1,2 +1,5 @@
 insert into users values (1, 'praj');
 insert into users values (2, 'harsh');
+
+insert into employee values (1, 'e1', 2000);
+insert into employee values (2, 'e2', 3000);
--- learn-spring-framework/src/main/resources/schema.sql
+++ learn-spring-framework/src/main/resources/schema.sql
@@ -3,3 +3,10 @@ create table users
     id  int,
     name   varchar(32)
 );
+
+create table employee
+(
+    id  int,
+    name   varchar(32),
+    salary integer
+);
