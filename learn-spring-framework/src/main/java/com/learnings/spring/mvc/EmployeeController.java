926040c393d9022c4de27ddfd57b20164459746c
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-04 07:05:24 +0530

    Add MvcMain class to initialize Spring Boot application

--- learn-spring-framework/src/main/java/com/learnings/spring/mvc/EmployeeController.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/mvc/EmployeeController.java
@@ -1,4 +1,24 @@
 package com.learnings.spring.mvc;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employee")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
 }
--- learn-spring-framework/src/main/java/com/learnings/spring/mvc/EmployeeService.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/mvc/EmployeeService.java
@@ -1,4 +1,18 @@
 package com.learnings.spring.mvc;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+    private final EmployeeDao employeeDao;
+
+    public EmployeeService(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeDao.findAll();
+    }
 }
