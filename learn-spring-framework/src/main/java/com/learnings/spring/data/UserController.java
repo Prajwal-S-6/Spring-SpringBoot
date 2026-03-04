4ee0b4058fc694616f7f36834b4c4949a5ea0a63
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-04 06:45:38 +0530

    Add UserDaoJpa interface for JPA repository support

--- learn-spring-framework/src/main/java/com/learnings/spring/data/UserController.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/data/UserController.java
@@ -3,10 +3,7 @@ package com.learnings.spring.data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +21,7 @@ public class UserController {
 
     @GetMapping("/count")
     @ResponseStatus(HttpStatus.CREATED)
-    public Integer getUsersCount() {
+    public Integer getUsersCount(@RequestParam String empId) {
         return userService.getUsersCount();
     }
 
