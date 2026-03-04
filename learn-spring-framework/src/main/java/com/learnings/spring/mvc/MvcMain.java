900a8359c19d25a55e0686c0c27ede17e415de15
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-04 06:57:55 +0530

    Add MvcMain class to initialize Spring Boot application

---
+++ learn-spring-framework/src/main/java/com/learnings/spring/mvc/MvcMain.java
@@ -0,0 +1,12 @@
+package com.learnings.spring.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcMain {
+
+    static void main(String[] args) {
+        SpringApplication.run(MvcMain.class, args);
+    }
+}
