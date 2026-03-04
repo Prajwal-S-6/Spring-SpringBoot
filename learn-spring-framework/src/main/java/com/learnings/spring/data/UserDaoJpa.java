cf495f35ba9bff9230fb338670ae77baef4be425
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-04 06:52:57 +0530

    Add UserDaoJpa interface for JPA repository support

--- learn-spring-framework/src/main/java/com/learnings/spring/data/UserDaoJpa.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/data/UserDaoJpa.java
@@ -1,8 +1,12 @@
 package com.learnings.spring.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDaoJpa extends JpaRepository<Users, Integer> {
+
+    @Query(value = "SELECT * from users where name=?1", nativeQuery = true)
+    public Users findByName(String name);
 }
