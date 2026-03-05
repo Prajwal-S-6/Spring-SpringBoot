2eb8c4dbe6a8151affbe4c9e7497c6c3f1051ca8
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-05 07:13:02 +0530

    Add SecurityMain class and spring-boot-starter-security dependency for application security

--- learn-spring-framework/src/main/java/com/learnings/spring/security/EmployeeService.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/security/EmployeeService.java
@@ -1,12 +1,16 @@
 package com.learnings.spring.security;
 
 import com.learnings.spring.mvc.EmployeeNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeService {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     private final EmployeeDao employeeDao;
 
     public EmployeeService(EmployeeDao employeeDao) {
@@ -39,5 +43,13 @@ public class EmployeeService {
     public void deleteEmployeeById(Integer id) {
         Employee employee = employeeDao.findById(id).orElseThrow(EmployeeNotFoundException::new);
         employeeDao.delete(employee);
+    }
+
+    public void registerEmployee(String name, String password, String role) {
+        Employee employee = new Employee();
+        employee.setName(name);
+        employee.setPassword(bCryptPasswordEncoder.encode(password));
+        employee.setRole(role);
+        employeeDao.save(employee);
     }
 }
--- learn-spring-framework/src/main/resources/data.sql
+++ learn-spring-framework/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into users values (1, 'praj');
 insert into users values (2, 'harsh');
 
-insert into employee values (1, 'e1', 2000, 'password');
-insert into employee values (2, 'e2', 3000, 'password');
-
-ALTER TABLE EMPLOYEE ALTER COLUMN id RESTART WITH 3;
+-- insert into employee values (1, 'e1', 2000, 'password', 'ADMIN');
+-- insert into employee values (2, 'e2', 3000, 'password', 'USER');
+--
+-- ALTER TABLE EMPLOYEE ALTER COLUMN id RESTART WITH 3;
