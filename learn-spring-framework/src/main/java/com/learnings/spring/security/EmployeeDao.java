c8dd7844fa2f52c0da53e921851f30c38e12d3e0
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-05 06:58:56 +0530

    Add SecurityMain class and spring-boot-starter-security dependency for application security

--- learn-spring-framework/src/main/java/com/learnings/spring/security/EmployeeDao.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/security/EmployeeDao.java
@@ -3,6 +3,9 @@ package com.learnings.spring.security;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeDao extends JpaRepository<Employee, Integer> {
+    Optional<Employee> findEmployeeByName(String name);
 }
