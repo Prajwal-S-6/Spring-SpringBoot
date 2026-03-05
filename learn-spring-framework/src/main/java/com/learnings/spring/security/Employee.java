79068f4aa4627f9073508dbe98d3a71fe48b8a06
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-05 06:36:50 +0530

    Add SecurityMain class and spring-boot-starter-security dependency for application security

---
+++ learn-spring-framework/src/main/java/com/learnings/spring/security/Employee.java
@@ -0,0 +1,20 @@
+package com.learnings.spring.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "employee")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    public Integer id;
+
+    public String name;
+
+    public Integer salary;
+}
---
+++ learn-spring-framework/src/main/java/com/learnings/spring/security/EmployeeController.java
@@ -0,0 +1,53 @@
+package com.learnings.spring.security;
+
+import com.learnings.spring.mvc.EmployeeNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employee")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping("employee")
+    public ResponseEntity<Object> addEmployee(@RequestBody Employee employee) {
+        Integer savedEmpId = employeeService.addEmployee(employee);
+        return ResponseEntity.created(UriComponentsBuilder.fromUriString("/api/employee/{savedEmpId}").build(savedEmpId)).build();
+    }
+
+    @PutMapping("employee/{empId}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("empId") Integer id, @RequestBody Employee employee) {
+        return ResponseEntity.ok(employeeService.updateEmployee(id, employee));
+    }
+
+    @PatchMapping("/employee")
+    public ResponseEntity<Employee> updateSalary(@RequestParam("id") Integer id, @RequestBody Employee employee) {
+        return ResponseEntity.ok(employeeService.updateSalary(id, employee));
+    }
+
+    @DeleteMapping("employee")
+    public ResponseEntity<Void> deleteEmployee(@RequestParam Integer id) {
+        employeeService.deleteEmployeeById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<String> employeeNotFound() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Employee not found");
+    }
+
+}
