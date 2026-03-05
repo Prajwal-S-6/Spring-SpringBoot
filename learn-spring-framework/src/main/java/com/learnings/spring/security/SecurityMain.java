1d3d840de59f15482f464963a6cd81647d6b2b9c
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-05 06:35:54 +0530

    Add SecurityMain class and spring-boot-starter-security dependency for application security

---
+++ learn-spring-framework/src/main/java/com/learnings/spring/security/SecurityMain.java
@@ -0,0 +1,12 @@
+package com.learnings.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMain {
+
+    static void main(String[] args) {
+        SpringApplication.run(SecurityMain.class, args);
+    }
+}
