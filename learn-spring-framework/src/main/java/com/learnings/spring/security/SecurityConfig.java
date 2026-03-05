50cf0da5df5b26be55f17a884e5506a97b69afd8
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-05 06:58:59 +0530

    Add SecurityMain class and spring-boot-starter-security dependency for application security

--- learn-spring-framework/src/main/java/com/learnings/spring/security/SecurityConfig.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.learnings.spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,7 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(auth ->
-                auth.requestMatchers("api/employee").permitAll())
+                auth.requestMatchers(HttpMethod.GET, "api/employee").hasRole("LIST_USER"))
                 .formLogin(Customizer.withDefaults()).build();
     }
 }
