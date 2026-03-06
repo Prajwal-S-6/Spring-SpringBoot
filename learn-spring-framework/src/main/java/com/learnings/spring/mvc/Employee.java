9e76aeb06e4c07f709fdb0d8f9f3c046d59d2fc7
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-06 07:34:44 +0530

    Add Lombok annotations for constructor generation in Employee class

--- learn-spring-framework/src/main/java/com/learnings/spring/mvc/Employee.java
+++ learn-spring-framework/src/main/java/com/learnings/spring/mvc/Employee.java
@@ -2,11 +2,15 @@ package com.learnings.spring.mvc;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "employee")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
 
     @Id
