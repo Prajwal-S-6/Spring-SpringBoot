5a8d0a009cfe1ba90770cb4a6999a1837d213513
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-03-06 07:39:46 +0530

    Add Lombok annotations for constructor generation in Employee class

---
+++ learn-spring-framework/src/test/java/com/learnings/spring/mvc/EmployeeServiceTest.java
@@ -0,0 +1,34 @@
+package com.learnings.spring.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+class EmployeeServiceTest {
+
+    @Mock
+    private EmployeeDao employeeDao;
+
+    @InjectMocks
+    private EmployeeService employeeService;
+
+    @Test
+    public void shouldGetAllEmployees() {
+        Mockito.when(employeeDao.findAll()).thenReturn(List.of(new Employee(1, "A", 200)));
+
+        List<Employee> employees = employeeService.getAllEmployees();
+
+        assertThat(employees.size()).isEqualTo(1);
+        assertThat(employees.getFirst().getName()).isEqualTo("A");
+
+    }
+}
